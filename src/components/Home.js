956c8f363054663b59aa4b8474f4f7859b5c8fd7
Mycroft1891 <mycroft1891@gmail.com>
2018-03-15 04:13:03 +0100

    handle user input logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import {Card, CardActions, CardHeader, CardTitle} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import { getQuestion, getRandomNumber } from '../data/Sign';
+import '../App.css';
 
 class Home extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			answer: '1',
-			hiragana: true,
+			answer: 1,
+			userChoice: 'hiragana',
 			lives: 5,
 			options: ['1','2','3','4'],
 			question: '1',
@@ -15,52 +15,46 @@ class Home extends Component {
 		}
 	}
 
-	componentWillMount() {
+	incrementScore() {
+		this.setState(prev => ({score: prev.score + 1}))
+	}
+
+	removeLife() {
+		this.setState(prev => ({lives: prev.lives - 1}))
+	}
+
+	pickNewQuestion() {
+		const response = getQuestion(this.state.userChoice)
+		let randomInt = getRandomNumber(4)
 		this.setState({
-			options: ['1','2','3','4'],
-			answer: '1'
+			options: response.options,
+			question: response.answers[randomInt],
+			answer: randomInt
 		})
 	}
 
-	getTitle() {
-		return this.props.hiragana
-		? 'Translate Hiragana to Romaji'
-		: 'Translate Katakana to Romaji'
-	}
-
-	getScore() {
-		return `Score: ${this.state.score}`
-	}
-
-	checkAnswer() {
-		this.setState(prev => {
-			score: prev.score + 1
-		})
+	check(choice) {
+		const { options, answer } = this.state
+		choice === options[answer] ? this.incrementScore() : this.removeLife()
+		this.pickNewQuestion()
 	}
 
 	render() {
+		const { score, lives, question, options, hiragana } = this.state
 		return(
-			<div>
-				<Card className="card">
-					<CardHeader
-						title={this.getScore()}
-						avatar="images/jsa-128.jpg"
-					/>
-					<CardTitle
-						title={this.state.question}
-						subtitle={this.getTitle()}
-						className="game-display"
-					/>
-					<CardActions>
-						{ this.state.options.map((option, id) =>
-							<FlatButton
-								key={id}
-								label={option}
-								onClick={this.checkAnswer()}
-							/>
-						)}
-					</CardActions>
-				</Card>
+			<div className="card">
+				<h2>Score: {score}</h2>
+				<h2>Lifes: {lives}</h2>
+				<h2 className="display">{question}</h2>
+				<h3>{hiragana ? 'Translate Hiragana' : 'Translate Katakana'}</h3>
+				<div className="buttonGroup">
+				{options.map((choice, id) =>
+					<button key={id} onClick={event => this.check(choice)}>{choice}</button>
+				)}
+				</div>
+				<h3>Choose what to learn</h3>
+				<button className="option" onClick={event => this.changeMode(true)}>Hiragana</button>
+				<button className="option" onClick={event => this.changeMode(false)}>Katakana</button>
 			</div>
 		)
 	}
